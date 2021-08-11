bc4f697b945b077317d05ecd6bdc2d2733b3d12c
Clombardi91 <chl9195@icloud.com>
2021-08-11 12:05:24 -0400

    started at-3-homepage ticket

--- durham/urls.py
+++ durham/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from places import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/index', views.index, name='index'),
+    
 ]
