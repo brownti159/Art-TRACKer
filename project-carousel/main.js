1dd9b9ad012585a51a1b48871383d3d4ebebd7e2
Kim Wheaton <kwheatonart@gmail.com>
2021-08-12 12:04:28 -0400

    basic format for carousel using Materialize

---
+++ project-carousel/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('.carousel');
+    let instances = M.Carousel.init(elems);
+    console.log("elems ", elems)
+    let instance = M.Carousel.getInstance(elems);
+    console.log("instance ", instance)
+});
+
+
+// instance.next();
+// instance.prev();
