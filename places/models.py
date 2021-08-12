9001b739e25506a3c734231bdd9c5a6a28060b8b
allison <allison.durham@gmail.com>
2021-08-12 17:10:13 -0400

    string method for rendering artist name insead of artist object

--- places/models.py
+++ places/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Artist(models.Model):
     name = models.CharField(max_length=200)
     website = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
