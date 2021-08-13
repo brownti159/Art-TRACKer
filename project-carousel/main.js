b06f3c5206f46bbb1b7de405e8110b7c82a5f218
Kim Wheaton <kwheatonart@gmail.com>
2021-08-13 12:17:18 -0400

    working slider with HTML, CSS and JS

--- project-carousel/main.js
+++ project-carousel/main.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     let elems = document.querySelectorAll('.carousel');
     let instances = M.Carousel.init(elems);
+    console.log("instances ", instances)
     console.log("elems ", elems)
+    
     let instance = M.Carousel.getInstance(elems);
     console.log("instance ", instance)
 });
