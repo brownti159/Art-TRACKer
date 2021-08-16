c3e88860e4287ea1bde22048295f95777f8ee67a
Clombardi91 <chl9195@icloud.com>
2021-08-16 11:26:53 -0400

    linked map.js to base.html

--- static/js/map.js
+++ static/js/map.js
@@ -1,8 +1,19 @@
-let map;
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2xvbWJhcmRpIiwiYSI6ImNrcnpuM3R1dTA4NmIydm9jdHVhNXFudGIifQ.Wuy9V0zOtcfLgdX7efVAWw';
+let map = new mapboxgl.Map({
+    center: [-78.9040881, 35.9964035,],
+    zoom: 10,
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
+
+});
+
+map.addControl(
+new MapboxGeocoder({
+accessToken: mapboxgl.accessToken,
+mapboxgl: mapboxgl
+}));
+
+const marker1 = new mapboxgl.Marker()
+.setLngLat([-78.9048045, 35.9968398])
+.addTo(map);
