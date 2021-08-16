1bd279c1415fee6b980456f946c617fd4fa1eee4
brownti159 <brownti159@gmail.com>
2021-08-16 17:22:32 -0400

    Added user model

--- places/models.py
+++ places/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+
 
 # Create your models here.
 class Artist(models.Model):
@@ -7,3 +11,9 @@ class Artist(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class User(AbstractUser):
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30,  blank=True)
+    birth_date = models.DateField(null=True, blank=True)
