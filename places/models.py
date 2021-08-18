946e1b3456562fc74aac156e6f79ec2e292e7908
allison <allison.durham@gmail.com>
2021-08-18 17:46:23 -0400

    removed website from Works, addressed other things I think

--- places/models.py
+++ places/models.py
@@ -5,7 +5,7 @@ from django.db.models.fields.related import ManyToManyField
 # Create your models here.
 class Artist(models.Model):
     name = models.CharField(max_length=200)
-    website = models.URLField(blank=True)
+    website = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -19,8 +19,7 @@ class Work(models.Model):
     category = models.CharField(max_length=20, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     # artistStatement = soundbyte?
-    photo = models.ImageField
-    artistWebsite = models.URLField(blank=True, null=True)
+    photo = models.ImageField(blank=True, null=True)
     notes = models.TextField(blank=True, null=True)
     wheelchairAccessible = models.BooleanField(blank=True, null=True)
 
