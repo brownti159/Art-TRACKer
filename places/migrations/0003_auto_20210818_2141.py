79042de66251efb6e803cb645c9e0910d204b011
allison <allison.durham@gmail.com>
2021-08-19 10:44:09 -0400

    hotfix migration dependencies

--- places/migrations/0003_auto_20210818_2141.py
+++ places/migrations/0003_auto_20210818_2141.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('places', '0002_work'),
+        ('places', '0002_auto_20210818_1426'),
     ]
 
     operations = [
