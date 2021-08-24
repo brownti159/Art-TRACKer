f1bf526ac406d6ea9a83075981b86abe8b465007
Kim Wheaton <kwheatonart@gmail.com>
2021-08-24 18:23:38 -0400

    trying to work out marker1

--- static/js/map.js
+++ static/js/map.js
@@ -35,36 +35,39 @@ let artWorks = [{
 const marker1 = new mapboxgl.Marker()
 .setLngLat([-78.9048045, 35.9968398])
 .addTo(map);
+console.log(marker1)
+marker1._element.addEventListener('click', (e) => {
+    console.log(e.point)
+        let popup = new mapboxgl.Popup({ offset: [0, -50] })       
+        .setLngLat([-78.9048045, 35.9968398])     
+        .setHTML(
+            '<h3>' + "Pleiades Gallery" + '</h3>' +
+            '<img src="./static/img/pleiades.jpg" width="200" height="150"></img>'
+            )
+        .addTo(map);
+})
 
 const marker2 = new mapboxgl.Marker()
 .setLngLat([-78.903233, 35.9971451])
 .addTo(map);
 
-function addPopup (workArray) {
-    map.on('click', (e) => {
-        let pinLat = e.lngLat.lat
-        let pinLng = e.lngLat.lng
-        let features = map.queryRenderedFeatures(e.point, {
-        });
-        if (!features.length) {
-            return;
-        }
-        let feature = features[0];
-        console.log(feature)
+// function addPopup (workArray) {
+//     map.on('click', (e) => {
+//         let pinLat = e.lngLat.lat
+//         let pinLng = e.lngLat.lng
+        // let features = map.queryRenderedFeatures(e.point, {
+        // });
+        // if (!features.length) {
+        //     return;
+        // }
+        // let feature = features[0];
+        // console.log(feature)
 
-        for (let work of workArray) {
-            console.log (pinLat, pinLng)
-            console.log(e)
-            if (pinLng === work.longitude && pinLat === work.latitude) {
-            let popup = new mapboxgl.Popup({ offset: [0, -50] })
-            .setLngLat([work.longitude, work.latitude])
-            .setHTML(
-                '<h3>' + `${work.title}` + '</h3>' +
-                `<img src=${work.photo} width="200" height="150"></img>`
-                )
-                .addTo(map);
-            }
-        };
-    });
-}
-        addPopup(artWorks)
+//         for (let work of workArray) {
+//             console.log (pinLat, pinLng)
+//             console.log(work.latitude, work.longitude)
+//             }
+//         };
+//     });
+// }
+//         addPopup(artWorks)
