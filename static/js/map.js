dca14e6750b54afaff556513d7a707f0e7d2626c
Kim Wheaton <kwheatonart@gmail.com>
2021-08-24 16:53:17 -0400

    work in progress with lngLat

--- static/js/map.js
+++ static/js/map.js
@@ -13,39 +13,58 @@ accessToken: mapboxgl.accessToken,
 mapboxgl: mapboxgl
 }));
 
-let artWorks = {
+let artWorks = [{
     "longitude" :  -78.9048045,
     "latitude" : 35.9968398,
     "title" : "Pleiades Gallery",
-    "year" : "2015", 
+    "year" : 2019, 
     "creators" : "Kim-n-friends",
-    "photo" : "./img/bull.jpeg",
-    "description" : "This was Kim's gallery."
-}
+    "description" : "This was Kim's gallery.",
+    "photo" : "./static/img/pleiades.jpeg",
+},
+{
+    "longitude" : -78.903233 ,
+    "latitude" : 35.9971451,
+    "title" : "Test Site",
+    "year" : 1821, 
+    "creators" : "artists",
+    "description" : "This is some art.",
+    "photo" : "./static/img/logo.jpg",
+}]
 
 const marker1 = new mapboxgl.Marker()
 .setLngLat([-78.9048045, 35.9968398])
 .addTo(map);
 
-function addPopup (work) {
+const marker2 = new mapboxgl.Marker()
+.setLngLat([-78.903233, 35.9971451])
+.addTo(map);
 
+function addPopup (workArray) {
     map.on('click', (e) => {
+        let pinLat = e.lngLat.lat
+        let pinLng = e.lngLat.lng
         let features = map.queryRenderedFeatures(e.point, {
         });
         if (!features.length) {
             return;
         }
         let feature = features[0];
-        
-        let popup1 = new mapboxgl.Popup({ offset: [0, -15] })
-        .setLngLat([work.longitude, work.latitude])
-        .setHTML(
-            '<h3>' + `${work.title}` + '</h3>' +
-            // '<img>' + feature.properties.image + '</img>' +
-            '<p>' + `${work.description}` + '</p>'
-            )
-            .addTo(map);
-        });
-}
+        console.log(feature)
 
-addPopup(artWorks)
+        for (let work of workArray) {
+            console.log (pinLat, pinLng)
+            console.log(e)
+            if (pinLng === work.longitude && pinLat === work.latitude) {
+            let popup = new mapboxgl.Popup({ offset: [0, -50] })
+            .setLngLat([work.longitude, work.latitude])
+            .setHTML(
+                '<h3>' + `${work.title}` + '</h3>' +
+                `<img src=${work.photo} width="200" height="150"></img>`
+                )
+                .addTo(map);
+            }
+        };
+    });
+}
+        addPopup(artWorks)
