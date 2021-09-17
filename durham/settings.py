a421e4d88dc4298c046527b56c1ab17cf5499bdd
Rebecca Conley <rebecca@momentumlearn.com>
2021-09-17 16:37:36 -0400

    add django-heroku to settings.py

--- durham/settings.py
+++ durham/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 
@@ -140,3 +141,5 @@ AUTH_USER_MODEL = 'places.User'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
